2f7da4ad597981bde3afd6ef8540ee87fa107196
Carlos Padilla <carlospadilla91@users.noreply.github.com>
2023-02-15 11:44:07 -0600

    feat: updated hibernate version in pom to fix error, added dao and udpated service to use dao

--- src/main/java/com/safeStops/model/SafeStop.java
+++ src/main/java/com/safeStops/model/SafeStop.java
@@ -1,10 +1,28 @@
 package com.safeStops.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "safestops")
 public class SafeStop {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@Column(name = "rating")
 	private Long rating;
 	
 	public SafeStop() {}
--- src/main/java/com/safeStops/service/SafeStopServiceImpl.java
+++ src/main/java/com/safeStops/service/SafeStopServiceImpl.java
@@ -1,60 +1,53 @@
 package com.safeStops.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.safeStops.dao.SafeStopDAO;
 import com.safeStops.model.SafeStop;
 
 @Service("safeStopService")
 public class SafeStopServiceImpl implements SafeStopService {
 	
-private static final AtomicLong counter = new AtomicLong();
+	@Autowired private SafeStopDAO safeStopsDAO;
 	
-	private static List<SafeStop> safeStops;
-	
-	static {
-		safeStops = populateDummySafeStops();
-	}
-	
+	@Override
+	@Transactional
 	public List<SafeStop> findAllSafeStops() {
-		return safeStops;
+		return safeStopsDAO.findAllSafeStops();
 	}
 	
-	public SafeStop findById(long id) {
-		for(SafeStop safeStop: safeStops) {
-			if(safeStop.getId() == id) return safeStop;
-		}
-		return null;
+	@Override
+	@Transactional
+	public SafeStop findById(Long id) {
+		return safeStopsDAO.findById(id);
 	}
 	
+	@Override
+	@Transactional
 	public SafeStop findByName(String name) {
-		for(SafeStop safeStop : safeStops) {
-			if(safeStop.getName().equalsIgnoreCase(name)) return safeStop;
-		}
-		return null;
+		return safeStopsDAO.findByName(name);
 	}
 	
+	@Override
+	@Transactional
 	public void saveSafeStop(SafeStop safeStop) {
-		safeStop.setId(counter.incrementAndGet());
-		safeStops.add(safeStop);
+		safeStopsDAO.saveSafeStop(safeStop);
 	}
 	
+	@Override
+	@Transactional
 	public void updateSafeStop(SafeStop safeStop) {
-		safeStops.set(safeStops.indexOf(safeStop), safeStop);
+		safeStopsDAO.updateSafeStop(safeStop);
 	}
 
-	public boolean isSafeStopExist(SafeStop safeStop) {
-		return findByName(safeStop.getName()) != null;
+	@Override
+	public void deleteSafeStop(Long id) {
+		safeStopsDAO.deleteSafeStop(id);
+		
 	}
-	
-	private static List<SafeStop> populateDummySafeStops(){
-        List<SafeStop> safeStops = new ArrayList<SafeStop>();
-//        safeStops.add(new SafeStop(counter.incrementAndGet(),"Buc-ees", "BEST OUT THERE!", 5L));
-//        safeStops.add(new SafeStop(counter.incrementAndGet(),"7-eleven", "Nice and lit at night", 4L));
-        return safeStops;
-    }
 
 }
